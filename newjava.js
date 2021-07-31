60a0a73373d20432bb9f7bfb07d25e455e67da93
Apurvashashank <vihaanapurva@gmail.com>
2021-07-31 14:40:47 +0530

    First commit

---
+++ newjava.js
@@ -0,0 +1,24 @@
+var button1=document.querySelector('#btn1')
+var button2=document.querySelector('#btn2')
+var button3=document.querySelector('#btn3')
+var button4=document.querySelector('#btn4')
+
+var body=document.querySelector('body')
+
+
+
+
+
+
+// button1.addEventListener('click',()=>{
+//     body.style.backgroundColor='chocolate'
+// })
+// button2.addEventListener('click',()=>{
+//     body.style.backgroundColor='orchid'
+// })
+// button3.addEventListener('click',()=>{
+//     body.style.backgroundColor='#2d2d2d'
+// })
+// button4.addEventListener('click',()=>{
+//     body.style.backgroundColor='chartreuse'
+// })
